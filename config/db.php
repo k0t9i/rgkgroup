ca4f6a06dafa4a847059759bbd5484269d661690
k0t9i <k0t9i@yandex.ru>
2016-05-17 22:48:23 +0500

    Changed db config

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=localhost;dbname=rgkgroup',
+    'username' => 'rgkgroup',
+    'password' => '1',
     'charset' => 'utf8',
 ];
