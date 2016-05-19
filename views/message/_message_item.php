19293d235ac69928bc67b36adea7b7629959373b
k0t9i <k0t9i@yandex.ru>
2016-05-19 11:11:35 +0300

    Added nl2br to message body

--- views/message/_message_item.php
+++ views/message/_message_item.php
@@ -5,7 +5,7 @@
             <span class="pull-right"><a href="<?=\yii\helpers\Url::to(['read', 'id' => $model->id, 'returnUrl' => $returnUrl])?>"><i class="glyphicon glyphicon-eye-open" data-pjax="0"></i></a></span>
         <?php endif ?>
     </div>
-    <div class="panel-body"><?= $model->body ?></div>
+    <div class="panel-body"><?= nl2br($model->body) ?></div>
     <div class="panel-footer clearfix">
         <span class="label label-default pull-left"><?=Yii::$app->formatter->asDateTime($model->createdAt)?></span>
         <span class="label label-primary pull-right">From: <?=$model->sender ? $model->sender->username : 'system'?></span>
