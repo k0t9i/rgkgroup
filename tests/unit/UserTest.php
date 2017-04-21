54ce8c3cc839487ae7e30fcf8b431acbcae2772f
k0t9i <k0t9i@yandex.ru>
2017-04-21 11:46:22 +0500

    Add unit tests for User model

--- tests/unit/UserTest.php
+++ tests/unit/UserTest.php
@@ -4,6 +4,8 @@ namespace app\tests;
 
 use app\models\User;
 use app\tests\fixtures\UserFixtures;
+use Codeception\Util\Stub;
+use yii\base\Security;
 
 class UserTest extends \Codeception\Test\Unit
 {
@@ -23,23 +25,57 @@ class UserTest extends \Codeception\Test\Unit
     public function testFindByIdentity()
     {
         $this->tester->haveFixtures(['users' => UserFixtures::className()]);
+        $userFixture = $this->tester->grabFixture('users', 0);
 
-        $this->tester->assertNotEmpty($user = User::findIdentity(1234));
-        $this->tester->assertEquals('test', $user->username);
+        $this->tester->assertNotEmpty($user = User::findIdentity($userFixture['id']));
+        $this->tester->assertEquals($userFixture['username'], $user->username);
 
-        $this->tester->assertEmpty(User::findIdentity(9999));
+        $this->tester->assertEmpty(User::findIdentity(-1));
     }
 
     public function testValidatePassword()
     {
-        $user = new User([
-            'passwordHash' => '$2y$13$.sPeWv2B/aTqg.yp9pim/Oqmdq0hI.acALy1FLFilut2XGHFtfFF.' // password123
-        ]);
-        $this->tester->assertTrue($user->validatePassword('password123'));
+        $expectedPassword = 'password123';
+        $expectedHash = '$2y$13$.sPeWv2B/aTqg.yp9pim/Oqmdq0hI.acALy1FLFilut2XGHFtfFF.';
 
-        $user = new User([
-            'passwordHash' => '$2y$13$.sPeWv2B/aTqg.yp9pim/Oqmdq0hI.acALy1FLFilut2XGHFtfFF.' // password123
+        /** @var Security $security */
+        $security = Stub::makeEmpty('yii\base\Security', [
+            'validatePassword' => function ($password, $hash) use ($expectedPassword, $expectedHash) {
+                return $password == $expectedPassword &&
+                    $hash == $expectedHash;
+            }
         ]);
-        $this->tester->assertFalse($user->validatePassword('password123invalid'));
+        $user = new User([
+            'passwordHash' => $expectedHash // password123
+        ]);
+        $this->tester->assertTrue($user->validatePassword($expectedPassword, $security));
+        $this->tester->assertFalse($user->validatePassword($expectedPassword . 'invalid', $security));
+    }
+
+    public function testFindIdentityByAccessToken()
+    {
+        $this->expectException('yii\base\NotSupportedException');
+        User::findIdentityByAccessToken('123');
+    }
+
+    public function testGetAuthKey()
+    {
+        $this->expectException('yii\base\NotSupportedException');
+        (new User())->getAuthKey();
+    }
+
+    public function testValidateAuthKey()
+    {
+        $this->expectException('yii\base\NotSupportedException');
+        (new User())->validateAuthKey('123');
+    }
+
+    public function testGetId()
+    {
+        $user = new User([
+            'id' => 2811
+        ]);
+        $this->tester->assertTrue($user->id == $user->getId());
+        $this->tester->assertFalse(-1 == $user->getId());
     }
 }
